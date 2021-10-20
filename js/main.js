a2cdd6a36811e119e16391741bf2ff61ef656060
ikhtiyorovich-pro <muhammadali.fayzullayev@yandex.ru>
2021-10-20 22:04:17 +0500

    next changes

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// console.log(fetch('http://www.omdbapi.com/?i=tt3896198&apikey=e959235f'));
+const elLoader = document.querySelector(".js-loader")
+const elFilmsList = document.querySelector(".js-films-list");
+
+fetch("http://www.omdbapi.com/?apikey=9fcd4d84&s=Avengers")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+
+    elLoader.style.display = "none"
+    working(data.Search);
+  });
+
+function working(array) {
+  array.forEach(element => {
+    renderFilms(element);
+  });
+}
+
+function renderFilms(object) {
+  const newLi = document.createElement("li");
+  const newHeading = document.createElement("h2")
+  const newPoster = document.createElement("img")
+  const newP = document.createElement("h4");
+  const newParagraphType = document.createElement("h5");
+
+  newLi.setAttribute("class", "list")
+  newPoster.setAttribute("class", "images")
+
+  newHeading.textContent = object.Title;
+  newPoster.src = object.Poster
+  newP.textContent = object.Year;
+  newParagraphType.textContent = object.Type;
+
+  elFilmsList.appendChild(newLi);
+  newLi.appendChild(newPoster)
+  newLi.appendChild(newHeading)
+  newLi.appendChild(newP);
+  newLi.appendChild(newParagraphType);
+}
